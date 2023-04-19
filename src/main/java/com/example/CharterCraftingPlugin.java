56866c6da06ad3c57488aaacfcdc9f04a3f5c29c
Sweed <Connorzane47@gmail.com>
2023-04-19 10:25:12 +1000

    Commit

--- src/main/java/com/example/CharterCraftingPlugin.java
+++ src/main/java/com/example/CharterCraftingPlugin.java
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Charter Crafting"
 )
-public class ExamplePlugin extends Plugin
+public class CharterCraftingPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private CharterCraftingConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+    CharterCraftingConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(CharterCraftingConfig.class);
 	}
 }
